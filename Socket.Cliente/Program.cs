9894a20555d23de66f4096c7e6be28255f1fc45d
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2021-12-20 13:47:52 +0100

    Agregar archivos de proyecto.

---
+++ Socket.Cliente/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+
+namespace Calculator.Cliente
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Console C#\r");
+            Console.WriteLine("------------------------\n");
+
+            while (true)
+            {
+                Console.WriteLine("Mensaje:");
+                string mensaje = Console.ReadLine();
+
+                var resultado = EnviaMenaje(mensaje);
+
+                Console.WriteLine(resultado);
+            }
+
+            Console.Write("Press any key to close the Calculator console app...");
+            Console.ReadKey();
+        }
+
+        static string EnviaMenaje(string mensaje)
+        {
+            try
+            {
+                // Connect to a Remote server
+                // Get Host IP Address that is used to establish a connection
+                // In this case, we get one IP address of localhost that is IP : 127.0.0.1
+                // If a host has multiple addresses, you will get a list of addresses
+                IPHostEntry host = Dns.GetHostEntry("localhost");
+                IPAddress ipAddress = host.AddressList[0];
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, 2800);
+
+                // Create a TCP/IP  socket.
+                using Socket sender = new Socket(ipAddress.AddressFamily,
+                    SocketType.Stream, ProtocolType.Tcp);
+
+                // Connect the socket to the remote endpoint. Catch any errors.
+                try
+                {
+                    // Connect to Remote EndPoint
+                    sender.Connect(remoteEP);
+
+                    Console.WriteLine("Socket connected to {0}",
+                        sender.RemoteEndPoint.ToString());
+
+                    Console.WriteLine("Socket redad for {0}",
+                        sender.LocalEndPoint.ToString());
+
+                    var cacheEnvio = Encoding.UTF8.GetBytes(mensaje);
+
+                    // Send the data through the socket.
+                    int bytesSend = sender.Send(cacheEnvio);
+
+                    // Receive the response from the remote device.
+                    byte[] bufferRec = new byte[1024];
+                    int bytesRec1 = sender.Receive(bufferRec);
+
+                    var resultado = Encoding.UTF8.GetString(bufferRec, 0, bytesRec1);
+
+                    // Release the socket.
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+
+                    return resultado;
+
+                }
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Socket.Servidor/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+
+namespace Calculator.Servidor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPHostEntry host = Dns.GetHostEntry("localhost");
+            IPAddress ipAddress = host.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 2800);
+
+            try
+            {
+                // Create a Socket that will use Tcp protocol
+                using Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                // A Socket must be associated with an endpoint using the Bind method
+                listener.Bind(localEndPoint);
+                // Specify how many requests a Socket can listen before it gives Server busy response.
+                // We will listen 10 requests at a time
+                listener.Listen(10);
+
+                Console.WriteLine("Waiting for a connection ..." + listener.LocalEndPoint.ToString());
+
+                while (true)
+                {
+                    Socket handler = listener.Accept();
+
+                    Console.WriteLine("Socket connected to {0}",
+                        handler.RemoteEndPoint.ToString());
+
+                    var cacheMenaje = new byte[4096];
+                    int bytesMenaje = handler.Receive(cacheMenaje);
+
+                    if (bytesMenaje > 0)
+                    {
+                        var mensaje = Encoding.UTF8.GetString(cacheMenaje, 0, bytesMenaje);
+
+                        var respuesta = "Ok: " + mensaje;
+
+                        Console.WriteLine("{0} -> {1}", mensaje, respuesta);
+
+                        var cacheRespuesta = Encoding.UTF8.GetBytes(respuesta);
+                        handler.Send(cacheRespuesta);
+                        Thread.Sleep(0);
+                    }
+
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("\n Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
